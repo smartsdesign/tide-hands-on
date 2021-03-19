050dd7f731cf8373eaeba0cab5f2cecf64a60089
Smson <samson.biz@gmail.com>
2021-03-19 08:53:03 +0000

    update starter files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { StorageService } from './storage.service';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  public title = 'tide-hands-on';
+export class AppComponent implements OnInit {
+  public title = 'Tide Hands-On Exercise';
   public loading: boolean = true;
 
-  public constructor(private _storageService: StorageService) {}
-
   public ngOnInit(): void {}
-
-  public ngAfterViewInit(): void {
-    // this.loading = false;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
