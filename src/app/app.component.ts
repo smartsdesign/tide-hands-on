0f64c6bb40f6f06cf6de2ee6d1c64dd1a5b093ec
Smson <samson.biz@gmail.com>
2021-03-19 09:41:21 +0000

    update debugging exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { StorageService } from './storage.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   public title = 'Tide Hands-On Exercise';
   public loading: boolean = true;
 
+  public constructor() {}
+
   public ngOnInit(): void {}
+
+  public ngAfterViewInit(): void {
+    this.loading = false;
+  }
 }
